c934910ab53e810163ae7a890b31e2cf4dd71c82
b-zelazko <bartlomiej.zelazko@gmail.com>
2026-03-06 11:56:15 +0100

    Add negative login tests for short username and password; keep BASE_URL as class variable; refactor successful login test

--- tests/test_login_demo_bank.py
+++ tests/test_login_demo_bank.py
@@ -1,12 +1,32 @@
 import pytest
 
 class TestLoginDemoBank:
+    BASE_URL = "https://demo-bank.vercel.app/"
 
     def test_successful_login(self, page):
         """Successful login with correct credentials"""
-        page.goto("https://demo-bank.vercel.app/")
+        page.goto(self.BASE_URL)
         page.get_by_test_id("login-input").fill("Tester01")
         page.get_by_test_id("password-input").fill("Pass0000")
         page.get_by_test_id("login-button").click()
 
         assert page.get_by_test_id("user-name").inner_text() == "Jan Demobankowy"
+
+
+    def test_login_with_short_username(self, page):
+        """Unsuccessful login with too short username"""
+        page.goto(self.BASE_URL)
+        page.get_by_test_id("login-input").fill("tester")
+        page.get_by_test_id("login-input").blur()
+
+        assert page.get_by_test_id("error-login-id").inner_text() == "identyfikator ma min. 8 znaków"
+
+
+    def test_login_with_short_password(self, page):
+        """Unsuccessful login with too short password"""
+        page.goto(self.BASE_URL)
+        page.get_by_test_id("login-input").fill("Tester01")
+        page.get_by_test_id("password-input").fill("123456")
+        page.get_by_test_id("password-input").blur()
+
+        assert page.get_by_test_id("error-login-password").inner_text() == "hasło ma min. 8 znaków"
