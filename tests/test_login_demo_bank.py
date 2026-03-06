58057564043441378d6f3d2a6452e4d854e39b37
b-zelazko <bartlomiej.zelazko@gmail.com>
2026-03-06 09:19:29 +0100

    Refactor to use pytest-playwright plugin and page fixture; group tests into TestLoginDemoBank class as preparation for adding negative login tests

--- tests/test_login_demo_bank.py
+++ tests/test_login_demo_bank.py
@@ -1,25 +1,12 @@
-from playwright.sync_api import Playwright, sync_playwright, expect
+import pytest
 
+class TestLoginDemoBank:
 
-def test_login_demo_bank():
-    with sync_playwright() as playwright:
-        browser = playwright.chromium.launch(headless=True)
-        context = browser.new_context()
-        page = context.new_page()
+    def test_successful_login(self, page):
+        """Successful login with correct credentials"""
         page.goto("https://demo-bank.vercel.app/")
-
-        # Login
         page.get_by_test_id("login-input").fill("Tester01")
         page.get_by_test_id("password-input").fill("Pass0000")
         page.get_by_test_id("login-button").click()
 
-        # Verify that the user has successfully logged in
         assert page.get_by_test_id("user-name").inner_text() == "Jan Demobankowy"
-
-        # ---------------------
-        context.close()
-        browser.close()
-
-# This ensures the test runs when executing the script directly without pytest
-if __name__ == "__main__":
-    test_login_demo_bank()
