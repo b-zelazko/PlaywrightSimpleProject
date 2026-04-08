e04e1126dfbd5225ec9954132d2f499519f46f24
b-zelazko <bartlomiej.zelazko@gmail.com>
2026-04-08 15:52:00 +0200

    Add 2 new mobile top-up tests.

--- pages/dashboard_page.py
+++ pages/dashboard_page.py
@@ -10,5 +10,17 @@ class DashboardPage:
         self.page.get_by_role("button", name="wykonaj").click()
         self.page.get_by_test_id("close-button").click()
 
+    def mobile_top_up(self, receiver, amount):
+        self.page.locator("#widget_1_topup_receiver").select_option(receiver)
+        self.page.locator("#widget_1_topup_amount").fill(amount)
+        self.page.locator("#uniform-widget_1_topup_agreement > span").click()
+        self.page.get_by_role("button", name="doładuj telefon").click()
+        self.page.get_by_test_id("close-button").click()
+
+    # --- GETTERS ---
+
     def get_message(self):
         return self.page.get_by_test_id("message-text").inner_text()
+
+    def get_balance(self):
+        return float(self.page.locator("#money_value").inner_text())
--- tests/test_dashboard.py
+++ tests/test_dashboard.py
@@ -20,3 +20,36 @@ class TestDashboard:
         # Assert
         assert dashboard.get_message() == \
             "Przelew wykonany! Chuck Demobankowy - 150,00PLN - zwrot kasy"
+
+    def test_successful_mobile_top_up(self, page):
+        # Arrange
+        login_page = LoginPage(page)
+        dashboard = DashboardPage(page)
+
+        login_page.open()
+        login_page.login("testerLO", "12345678")
+
+        # Act
+        dashboard.mobile_top_up("500 xxx xxx", "100")
+
+        # Assert
+        assert dashboard.get_message() == \
+            "Doładowanie wykonane! 100,00PLN na numer 500 xxx xxx"
+
+
+    def test_correct_balance_after_mobile_top_up(self, page):
+        # Arrange
+        login_page = LoginPage(page)
+        dashboard = DashboardPage(page)
+
+        login_page.open()
+        login_page.login("testerLO", "12345678")
+
+        initial_balance = dashboard.get_balance()
+        top_up_amount = 100
+
+        # Act
+        dashboard.mobile_top_up("500 xxx xxx", str(top_up_amount))
+
+        # Assert
+        assert dashboard.get_balance() == initial_balance - top_up_amount
